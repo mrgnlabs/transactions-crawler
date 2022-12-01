9bba7041b2e140cce7cf07333cf878981fe20c17
man0s <95379755+losman0s@users.noreply.github.com>
2022-12-01 13:49:01 +0800

    add Target string parsing

--- src/common.rs
+++ src/common.rs
@@ -1,11 +1,31 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use solana_sdk::{pubkey::Pubkey, signature::Signature};
+use std::str::FromStr;
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Target {
     pub address: Pubkey,
     pub before: Option<Signature>,
     pub until: Option<Signature>,
+}
+
+// Allows to parse a JSON target with base58-encoded addresses/sigs (serde expects byte arrays)
+impl FromStr for Target {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let targets_raw = json::parse(s).unwrap();
+
+        Ok(Self {
+            address: Pubkey::from_str(targets_raw["address"].as_str().unwrap()).unwrap(),
+            before: targets_raw["before"]
+                .as_str()
+                .map(|sig_str| Signature::from_str(sig_str).unwrap()),
+            until: targets_raw["until"]
+                .as_str()
+                .map(|sig_str| Signature::from_str(sig_str).unwrap()),
+        })
+    }
+
+    type Err = anyhow::Error;
 }
 
 pub const DEFAULT_RPC_ENDPOINT: &str = "https://api.mainnet-beta.solana.com";
